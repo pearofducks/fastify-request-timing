c1450bb63d171197daf1125d8c38aad5cbfdcfab
Dave Honneffer <pearofducks@gmail.com>
2021-05-26 14:14:37 +0200

    Fix regression vs. Fastify 3.16 and later

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import fp from 'fastify-plugin'
 
 export default fp((fastify, _, done) => {
-  fastify.addHook('onResponse', (_, reply) => reply.getResponseTime()) // this has to be called in this hook - to enable all the other getResponseTime calls
-  fastify.addHook('onSend', (_, reply) => reply.header('X-Response-Time', reply.getResponseTime()))
+  fastify.addHook('onResponse', async (_, reply) => {
+    reply.getResponseTime()
+  }) // this has to be called in this hook - to enable all the other getResponseTime calls
+  fastify.addHook('onSend', async (_, reply, payload) => {
+    reply.header('X-Response-Time', reply.getResponseTime())
+    return payload
+  })
   done()
 }, { fastify: '3.x', name: 'fastify-response-timing' })
