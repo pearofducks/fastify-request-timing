cdce201d62cb229c2659f414696e28374c49dcde
Dave Honneffer <pearofducks@gmail.com>
2024-02-01 12:31:48 +0100

    v3 - drop cjs and get ready for Fastify@5

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import fp from 'fastify-plugin'
 
 export default fp(async fastify => {
   fastify.addHook('onResponse', async (_, reply) => {
-    reply.getResponseTime()
+    reply.elapsedTime
   }) // this has to be called in this hook - to enable all the other getResponseTime calls
   fastify.addHook('onSend', async (_, reply, payload) => {
-    reply.header('X-Response-Time', reply.getResponseTime())
+    reply.header('X-Response-Time', reply.elapsedTime)
     return payload
   })
-}, { fastify: '3.x || 4.x', name: 'fastify-response-timing' })
+}, { fastify: '4.x || 5.x', name: 'fastify-response-timing' })
--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  input: './index.js',
-  output: {
-    exports: 'default',
-    file: 'dist/index.cjs',
-    format: 'cjs'
-  },
-  external: ['fastify-plugin']
-}
